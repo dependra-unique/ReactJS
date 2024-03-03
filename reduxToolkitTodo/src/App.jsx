406b8646ffa920a406c95d7a9d22266bc892948c
Dependra Kumar <dependragitcreator@gmail.com>
2024-03-03 16:58:20 +0530

    redux toolkit todo app done

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import AddTodo from './components/AddTodo'
 import Todos from './components/Todos'
+
 
 
 
