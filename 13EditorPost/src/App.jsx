b693ba5c7ab00fb209a2f69a637331b77c1e8466
Dependra Kumar <dependragitcreator@gmail.com>
2024-03-14 18:21:03 +0530

    Editor display

---
+++ 13EditorPost/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import EditorRTE from './components/EditorRTE'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>This is editor</h1>
+      <EditorRTE />
+    </>
+  )
+}
+
+export default App
