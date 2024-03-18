bd27e50c7cc766547c75eb4da6fa840a2ff364b9
Dependra Kumar <dependragitcreator@gmail.com>
2024-03-18 23:29:11 +0530

    redux toolkit

--- reduxToolkitTodo/src/components/AddTodo.jsx
+++ reduxToolkitTodo/src/components/AddTodo.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import {useDispatch} from 'react-redux'
 import {addTodo} from '../features/todo/todoSlice'
 
-
+//redux toolkit uses
 //AddTodo, matlab store mein kuch add karna hai 
 // store mein add dispatch method se karte hai (means koi action hume dispatch karna padega)
 function AddTodo() {
